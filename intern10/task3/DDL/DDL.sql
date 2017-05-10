e195f8ff39a589e69978b2e8cc6ed3b80f9a42af
alexandrapandra <alexandra.pandra@ibm.com>
2017-05-10 14:52:01 +0300

    ALTER TABLE orders CREATE TABLE printed_orders CREATE SEQUENCE printed_id_seq

--- intern10/task3/DDL/DDL.sql
+++ intern10/task3/DDL/DDL.sql
@@ -15,4 +15,23 @@ CREATE TABLE errors
 CREATE SEQUENCE log_seq
 START WITH 1
 INCREMENT BY 1;
+
+ALTER TABLE ORDERS
+DROP COLUMN PRINTED;
+DROP SEQUENCE printed_id_seq;
+DROP TABLE printed_orders;
+
+ALTER TABLE orders
+    ADD printed NUMBER(1) DEFAULT 0
+    CONSTRAINT chk_val CHECK(printed = 0 OR printed = 1);
+
+CREATE TABLE printed_orders(id NUMBER, 
+                            details CLOB
+                            );
+
+CREATE SEQUENCE printed_id_seq
+START WITH 1
+INCREMENT BY 1;
 /
+
+
