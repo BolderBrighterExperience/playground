57333df650ccc183268dd1ac43709d714f04ded5
DiandraTudorache <Diandra.Alexandra.Tudorache@ibm.com>
2017-05-10 16:57:48 +0300

    Adaugare Proceduri load_order si print_order

--- intern01/PLSQL/DT_Project/Packages/PKG_unit_testing_header.sql
+++ intern01/PLSQL/DT_Project/Packages/PKG_unit_testing_header.sql
@@ -5,7 +5,9 @@ PROCEDURE prc_unit_adjust_employee_data;
 PROCEDURE prc_unit_remove_employee;
 PROCEDURE prc_unit_add_department;
 PROCEDURE prc_unit_adjust_dep_data;
-PROCEDURE prc_unit_remove_department ;
+PROCEDURE prc_unit_remove_department;
+PROCEDURE prc_unit_load_order;
+PROCEDURE prc_unit_print_order;
   
 END  pkg_unit_testing;
 /
