076771c7f66f48adc30e3c5f1861f9bf57e752cd
DiandraTudorache <Diandra.Alexandra.Tudorache@ibm.com>
2017-05-23 16:53:38 +0300

    Exercitiu 11

---
+++ intern01/Test/Exercise11_test.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE PROCEDURE plch_show_customers(an_cust VARCHAR2)
+IS
+    TYPE t_bulk_collect IS TABLE OF plch_customers.custname%TYPE;
+    lt_bulk_collect t_bulk_collect;
+   
+BEGIN
+    EXECUTE IMMEDIATE an_cust 
+    BULK COLLECT INTO lt_bulk_collect;
+
+    FOR i in lt_bulk_collect.FIRST..lt_bulk_collect.LAST
+    LOOP
+    DBMS_OUTPUT.PUT_LINE(lt_bulk_collect(i));
+
+    END LOOP;
+END plch_show_customers;
+/ 
+
+BEGIN
+   plch_show_customers (
+     'select custname from plch_customers order by custname');
+END;
+/
