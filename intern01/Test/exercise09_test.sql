b8b0b05355af1e3773ee58ef36e52f126c28d27e
DiandraTudorache <Diandra.Alexandra.Tudorache@ibm.com>
2017-05-23 16:52:28 +0300

    Exercitiu 9

---
+++ intern01/Test/exercise09_test.sql
@@ -0,0 +1,17 @@
+declare
+   type numbers_t is table of number;
+   numbers  numbers_t := numbers_t( 2, 4, null, 8, 10 );
+   idx      number;
+begin
+   idx := 1;
+     WHILE  (numbers(idx) <= 8  or numbers(idx) is null)
+     loop
+      if numbers(idx) is null then
+         dbms_output.put_line('null');
+      else
+         dbms_output.put_line(numbers(idx));
+      end if;
+      idx := idx + 1;
+   end loop;
+end;
+/
