7724f585b02e1ed652885d2167da4089c222cb89
DiandraTudorache <Diandra.Alexandra.Tudorache@ibm.com>
2017-05-24 10:29:56 +0300

    Modificare variabila

--- intern01/Test/Exercise11_test.sql
+++ intern01/Test/Exercise11_test.sql
@@ -1,15 +1,15 @@
-CREATE OR REPLACE PROCEDURE plch_show_customers(an_cust VARCHAR2)
+CREATE OR REPLACE PROCEDURE plch_show_customers(as_cust VARCHAR2)
 IS
     TYPE t_bulk_collect IS TABLE OF plch_customers.custname%TYPE;
     lt_bulk_collect t_bulk_collect;
    
 BEGIN
-    EXECUTE IMMEDIATE an_cust 
+    EXECUTE IMMEDIATE as_cust 
     BULK COLLECT INTO lt_bulk_collect;
 
     FOR i in lt_bulk_collect.FIRST..lt_bulk_collect.LAST
     LOOP
-    DBMS_OUTPUT.PUT_LINE(lt_bulk_collect(i));
+        DBMS_OUTPUT.PUT_LINE(lt_bulk_collect(i));
 
     END LOOP;
 END plch_show_customers;
